91c834a68c34d6af23ed638a8b065c79257b2433
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 03:42:52 +0000

    [automatic commit] 2025-06-20 03:42:52 UTC

--- part1/db.js
+++ part1/db.js
@@ -8,3 +8,11 @@ const db = mysql.createPool({
     database: 'textbook_marketplace'
 });
 module.exports = db;
+
+CREATE TABLE Dogs (
+    dog_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    size ENUM('small', 'medium', 'large') NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+);
