67ac3d393be18e13142e2ecdaf79771c19705670
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 05:31:06 +0000

    [automatic commit] 2025-06-20 05:31:06 UTC

--- part1/db.js
+++ part1/db.js
@@ -1,10 +1,10 @@
-// const mysql = require( 'mysql2/promise');
+const mysql = require( 'mysql2/promise');
 
-// const db = mysql.createPool({
-//     // socketPath: '/var/run/mysqld/mysqlx.sock',
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'DogWalkService'
-// });
-// module.exports = db;
+const db = mysql.createPool({
+    // socketPath: '/var/run/mysqld/mysqlx.sock',
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'DogWalkService'
+});
+module.exports = db;
--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const router = express.Router();
-// const db = require('../db');
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     const [rows] = await db.query(`
-//       SELECT d.name AS dog_name, d.size, u.username AS owner_username
-//       FROM Dogs d
-//       JOIN Users u ON d.owner_id = u.user_id
-//     `);
-//     res.json(rows);
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
