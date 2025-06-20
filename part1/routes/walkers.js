1c48c0ab69c24da1b301fa5a3036aba76921cb44
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 04:03:31 +0000

    [automatic commit] 2025-06-20 04:03:31 UTC

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -8,11 +8,9 @@ router.get('/summary', async (req, res) => {
       SELECT
         u.username AS walker_username,
         COUNT(r.rating_id) AS total_ratings,
-        ROUND(AVG(r.rating), 1) AS average_rating,
-        COUNT(wr.request_id) AS completed_walks
+        ROUND(AVG(r.rating), 1) AS average_rating
       FROM Users u
       LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
-      LEFT JOIN WalkRequests wr ON wr.status = 'completed' AND wr.walker_id = u.user_id
       WHERE u.role = 'walker'
       GROUP BY u.user_id
     `);
