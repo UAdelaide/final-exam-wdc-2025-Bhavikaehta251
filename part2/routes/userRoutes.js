1f29d4903108f3bad23c05527c4ff9ba253061cf
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 15:49:02 +0000

    [automatic commit] 2025-06-20 15:49:02 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,7 +58,7 @@ router.post('/login', async (req, res) => {
 router.get('/me', async (req, res) => {
   try {
     const username = req.session.loggedInUser || null;
-    res.json(req.session.loggedInUser)
+    res.json({req.session.loggedInUser)
 
     if (!username) return res.status(401).json({ error: 'Not logged in' });
 
