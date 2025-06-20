3d6cd6a6f9c10c4445b481ce52e5cb687b7c23fe
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 11:39:19 +0000

    [automatic commit] 2025-06-20 11:39:19 UTC

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -4,16 +4,18 @@ const db = require('../db');
 
 router.get('/summary', async (req, res) => {
     try{
-        const[rows] = await db.query(`
-      SELECT
-        u.username AS walker_username,
-        COUNT(r.rating_id) AS total_ratings,
-        ROUND(AVG(r.rating), 1) AS average_rating
-      FROM Users u
-      LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
-      WHERE u.role = 'walker'
-      GROUP BY u.user_id
-    `);
+        const[rows] = await db.query(`SELECT
+    u.username AS walker_username,
+    COUNT(DISTINCT r.rating_id) AS total_ratings,
+    ROUND(AVG(r.rating), 1) AS average_rating,
+    COUNT(DISTINCT wr.request_id) AS completed_walks
+  FROM Users u
+  LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+  LEFT JOIN WalkRequests wr ON wr.request_id = r.request_id
+                             AND wr.status = 'completed'
+                             AND u.user_id = r.walker_id
+  WHERE u.role = 'walker'
+  GROUP BY u.user_id`);
     res.json(rows);
     }
     catch(err){
