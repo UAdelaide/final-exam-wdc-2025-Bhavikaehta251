097bd1dcd872025c28a9b831a0592501cd61bba3
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 05:51:15 +0000

    [automatic commit] 2025-06-20 05:51:15 UTC

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -5,9 +5,7 @@ const db = require('../db');
 router.get('/',async(req, res) => {
     try {
         const [rows] = await db.query('
-            SELECT d.name AS dog_name, d.size, u.username AS owner_username
-      FROM Dogs d
-      JOIN Users u ON d.owner_id = u.user_id
+            SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id
 ');
     res.json(rows)}
 }
