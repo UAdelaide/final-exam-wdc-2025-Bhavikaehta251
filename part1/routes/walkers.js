a40eec1376fa4841dca694c93638933fec1f6813
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 06:35:30 +0000

    [automatic commit] 2025-06-20 06:35:30 UTC

--- part1/routes/walkers.js
+++ part1/routes/walkers.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/summary', async (req, res) = >
+router.get('/summary', async (req, res) => {
+    
+}
 
 );
