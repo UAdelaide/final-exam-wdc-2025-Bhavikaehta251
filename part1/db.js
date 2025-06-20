57c963496ccac4c5392356e20b59eb7064acb588
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 03:43:58 +0000

    [automatic commit] 2025-06-20 03:43:58 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/db.js
+++ part1/db.js
@@ -8,16 +8,3 @@ const db = mysql.createPool({
     database: 'textbook_marketplace'
 });
 module.exports = db;
-
-// db.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
-
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-module.exports = pool;
