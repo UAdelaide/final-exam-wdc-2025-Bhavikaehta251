ae9b242a2065e794c84d59d857a7ce8ec4d8b00a
Bhavikamehta251 <bhavikabenvishnubhai.mehta@student.adelaide.edu.au>
2025-06-20 13:49:39 +0000

    [automatic commit] 2025-06-20 13:49:39 UTC

--- part2/routes/dogsRoutes.js
+++ part2/routes/dogsRoutes.js
@@ -6,12 +6,8 @@ const db = require('../models/db');
 router.get('/', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
-      FROM WalkRequests wr
-      JOIN Dogs d ON wr.dog_id = d.dog_id
-      JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'
-    `);
+      SELECT * from Dogs
+      `);
     res.json(rows);
   } catch (error) {
     console.error('SQL Error:', error);
